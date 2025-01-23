9f6e10fdceb709fbaef40a53ee75f4dae62bc324
ila36IX <jbariali002@gmail.com>
2025-01-23 17:24:36 +0100

    Turk algo takes 78486 what is this

--- push_swap/main.c
+++ push_swap/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aljbari <aljbari@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 14:42:41 by aljbari           #+#    #+#             */
+/*   Updated: 2025/01/23 17:22:47 by aljbari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/libft.h"
 #include "libft/printf/ft_printf.h"
 #include <bits/types/stack_t.h>
@@ -8,7 +20,8 @@
 typedef struct t_stack_ {
 	t_list *head;
 	t_list *tail;
-	int size; } t_stack;
+	int size; 
+} t_stack;
 
 void print_ints(int *arr, size_t size);
 void swap(int* a, int* b);
@@ -125,29 +138,46 @@ void print_arr(t_list *head)
  *
  * Return: Pointer to the head of the array, must be free later
  */ 
+/*t_list *arr_to_list(char **arr, int size)*/
+/*{*/
+/*	t_list *head;	*/
+/*	int *sorted_buff;	*/
+/*	int *item;	*/
+/*	int i;*/
+/**/
+/*	head = NULL;*/
+/*	i = 0;*/
+/*	sorted_buff = ft_calloc(sizeof(int), size);*/
+/*	if (!sorted_buff)*/
+/*		return (NULL);*/
+/*	while (i < size)*/
+/*	{*/
+/*		item = ft_calloc(sizeof(int), 1);*/
+/*		*item = ft_atoi(arr[i]);*/
+/*		sorted_buff[i] = *item;*/
+/*		ft_lstadd_back(&head, ft_lstnew(item));*/
+/*		i++;*/
+/*	}*/
+/*	quick_sort(sorted_buff, 0, size - 1);*/
+/*	normalize_arr(sorted_buff, head, size);*/
+/*	free(sorted_buff);*/
+/*	return (head);*/
+/*}*/
 t_list *arr_to_list(char **arr, int size)
 {
 	t_list *head;	
-	int *sorted_buff;	
 	int *item;	
 	int i;
 
 	head = NULL;
 	i = 0;
-	sorted_buff = ft_calloc(sizeof(int), size);
-	if (!sorted_buff)
-		return (NULL);
 	while (i < size)
 	{
 		item = ft_calloc(sizeof(int), 1);
 		*item = ft_atoi(arr[i]);
-		sorted_buff[i] = *item;
 		ft_lstadd_back(&head, ft_lstnew(item));
 		i++;
 	}
-	quick_sort(sorted_buff, 0, size - 1);
-	normalize_arr(sorted_buff, head, size);
-	free(sorted_buff);
 	return (head);
 }
 
@@ -411,12 +441,12 @@ int many_operations(t_stack *b, int value)
 	if (get_value(walk) > value)
 		return (1);
 	i = 1;
-	printf("Pushing %d\n", value);
+	/*printf("Pushing %d\n", value);*/
 	while(walk)
 	{
 		if (!walk->next)
 			return (-1);
-		printf("is %d between %d and %d?\n", value, get_value(walk), get_value(walk->next));
+		/*printf("is %d between %d and %d?\n", value, get_value(walk), get_value(walk->next));*/
 		if (get_value(walk) < value && get_value(walk->next) > value)
 				return (i);
 		i++;
@@ -526,60 +556,90 @@ int best_push_b(t_stack *b, int value)
 }
 
 
-void best_fit(t_stack *a, t_stack *b)
+int closest_smaller(t_stack *b, int n)
 {
-	int value;
-	int op_count;
 	t_list *walk;
-	t_list *curr;
+	int i;
+	int closest;
+	int closest_index;
 
-	walk = a->head;
+	/*printf("Find the [%d] target\n", n);*/
+	i = 0;
+	closest_index = -1;
+	walk = b->head;
 	while (walk)
 	{
-		curr = walk;
+		if (get_value(walk) < n)
+		{
+			closest_index = i;
+			closest = get_value(walk);
+			break;
+		}
+		i++;
 		walk = walk->next;
-		value = get_value(curr);
-		op_count = best_push_b(b, value);
-		// printf("%d needs %d ops;\n", value, op_count);
-		print_ab(a, b);
-		if (op_count > 0)
-		{
-			int i = op_count;
-			while (i > 1)
-			{
-				rotate_b(b);
-				i--;
-			}
-			i = op_count;
-			push_b(a, b);
-			while (i > 1)
-			{
-				rev_rotate_b(b);
-				i--;
-			}
-		}
-		if (op_count <= 0)
-		{
-			int i = op_count;
-			while (i < -1)
-			{
-				rev_rotate_b(b);
-				i++;
-			}
-			i = op_count;
-			push_b(a, b);
-			while (i < 0)
-			{
-				rotate_b(b);
-				i++;
-			}
-		}
-		print_ab(a,  b);
 	}
-
+	while (walk)
+	{
+		if (get_value(walk) < n && n - get_value(walk) < n - closest)
+		{
+			closest_index = i;
+			closest = get_value(walk);
+		}
+		i++;
+		walk = walk->next;
+	}
+	/*printf("[%d] target is in b[%d]\n", n, closest_index);*/
+	return (closest_index);
 }
 
-int closest_smaller(t_stack *a, int n)
+int smallest_index(t_stack *a)
+{
+	int i;
+	int small;
+	int small_idx;
+	t_list *walk;
+
+	walk = a->head;
+	small = get_value(walk);
+	small_idx = 0;
+	i = 0;
+	while (walk)
+	{
+		if (get_value(walk) < small)
+		{
+			small = get_value(walk);
+			small_idx = i;
+		}
+		i++;
+		walk = walk->next;
+	}
+	return (small_idx);
+}
+
+int bigger_index(t_stack *a)
+{
+	int i;
+	int big;
+	int big_idx;
+	t_list *walk;
+
+	walk = a->head;
+	big = get_value(walk);
+	i = 0;
+	while (walk)
+	{
+		if (get_value(walk) > big)
+		{
+			big = get_value(walk);
+			big_idx = i;
+		}
+		i++;
+		walk = walk->next;
+	}
+	return (big_idx);
+}
+
+int closest_bigger(t_stack *a, int n)
 {
 	t_list *walk;
 	int i;
@@ -588,12 +648,13 @@ int closest_smaller(t_stack *a, int n)
 
 	i = 0;
 	closest_index = -1;
+	walk = a->head;
 	while (walk)
 	{
-		if (get_value(walk->content) < n)
+		if (get_value(walk) > n)
 		{
 			closest_index = i;
-			closest = get_value(walk->content);
+			closest = get_value(walk);
 			break;
 		}
 		i++;
@@ -601,32 +662,263 @@ int closest_smaller(t_stack *a, int n)
 	}
 	while (walk)
 	{
-		if (get_value(walk->content) < n && n - get_value(walk->next) < n - closest)
+		if (get_value(walk) > n && get_value(walk) - n < closest - n)
 		{
 			closest_index = i;
-			closest = get_value(walk->content);
+			closest = get_value(walk);
 		}
 		i++;
 		walk = walk->next;
 	}
+	/*printf("target of %d is in a[%d]\n", n, closest_index);*/
 	return (closest_index);
 }
 
-void join(t_stack *a, t_stack *b)
+/*
+ * How many operations needed to bring the integer
+ * located in giving index to top of a
+ */
+int calc_ops(t_stack *a, int index)
 {
+	if (index > a->size / 2)
+		return -(a->size - index);
+	else
+		return (index);
+}
+
+
+/**
+ *
+ * _abs - absolute value of an integer n
+ *
+ * @n: integer
+ * Return: The absolute value of integer (it's positive version)
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+
+}
+
+
+/**
+ *
+ * optimized_ops_nbr - calculte the must optimal number of
+ * opetations to move two integers from a and b into the top 
+ * of there stack
+ * @a_idx: index of the first number at stack a
+ * @b_idx: index of the second number at stack b
+ *
+ * Return: The must optimal number of operations
+ */
+int optimized_ops_nbr(t_stack *a, t_stack *b, int a_idx, int b_idx)
+{
+	a_idx = calc_ops(a, a_idx);
+	b_idx = calc_ops(b, b_idx);
+
+	if (a_idx > 0 && b_idx > 0)
+	{
+		if (a_idx > b_idx)
+			return (b_idx + _abs(a_idx - b_idx));
+		else
+			return (a_idx + _abs(a_idx - b_idx));
+	}
+	else if (a_idx < 0 && b_idx < 0)
+	{
+		if (_abs(a_idx) > _abs(b_idx))
+			return (b_idx * -1 + _abs(a_idx - b_idx));
+		else
+			return (a_idx * -1 + _abs(a_idx - b_idx));
+	}
+	else
+		return (_abs(a_idx) + _abs(b_idx));
+}
+
+void excute_without_optimizing(t_stack *a, t_stack *b, int a_ops, int b_ops)
+{
+	if (a_ops > 0)
+		while (a_ops > 0)
+		{
+			a_ops--;
+			rotate_a(a);
+		}
+	else
+		while (a_ops < 0)
+		{
+			a_ops--;
+			rev_rotate_a(a);
+		}
+	if (b_ops > 0)
+		while (b_ops > 0)
+		{
+			b_ops--; rotate_b(b); } else
+		while (b_ops < 0)
+		{
+			b_ops++;
+			rev_rotate_b(a);
+		}
+}
+
+void sort3_a(t_stack *a)
+{
+	int n1;
+	int n2;
+	int n3;
+
+	if (a->size != 3)
+		return ;
+	n1 = get_value(a->head);
+	n2 = get_value(a->head->next);
+	n3 = get_value(a->head->next->next);
+	if (n2 > n1 && n2 > n3)
+		rev_rotate_a(a);
+	if (n1 > n2 && n1 > n3)
+		rotate_a(a);
+	n1 = get_value(a->head);
+	n2 = get_value(a->head->next);
+	if (n1 > n2)
+		swap_a(a);
+}
+
+void execute_ops(t_stack *a, t_stack *b, int a_idx, int b_idx)
+{
+	int a_ops = calc_ops(a, a_idx);
+	int b_ops = calc_ops(b, b_idx);
+
+	if (a_ops > 0 && b_ops > 0)
+		while (a_ops > 0 || b_ops > 0)
+		{
+			if (a_ops > 0 && b_ops > 0)
+				rotate_ab(a, b);
+			else if (a_ops > 0)
+				rotate_a(a);
+			else
+				rotate_b(b);
+			a_ops--;
+			b_ops--;
+		}
+	else if (a_ops < 0 && b_ops < 0)
+	{
+		a_ops = _abs(a_ops);
+		b_ops = _abs(b_ops);
+		while (a_ops > 0 || b_ops > 0)
+		{
+			if (a_ops > 0 && b_ops > 0)
+				rev_rotate_ab(a, b);
+			else if (a_ops > 0)
+				rev_rotate_a(a);
+			else
+				rev_rotate_b(b);
+			a_ops--;
+			b_ops--;
+		}
+	}
+	else
+		excute_without_optimizing(a, b, a_idx, b_idx);
+}
+
+void cheapest_push(t_stack *a, t_stack *b)
+{
+	t_list *walk;
+	int i, j, sum;
+	int best_comb[2];
+
+	walk = a->head;
+	i = 0;
+	sum = a->size + b->size;
+	while (walk)
+	{
+		j = closest_smaller(b, get_value(walk));
+		if (j == -1)
+		{
+			j = bigger_index(b);
+			/*printf("bigger: %d\n", j);*/
+		}
+		/*printf("a(i) = %d, b(i) = %d\n", i, j);*/
+		if (optimized_ops_nbr(a, b, i, j) < sum)
+		{
+			best_comb[0] = i;
+			best_comb[1] = j;
+			
+			sum = optimized_ops_nbr(a, b, i, j);
+			/*printf("{new best} a(i) = %d, b(i) = %d\n", i, j);*/
+		}
+		if (sum == 0)
+			break;
+		walk = walk->next;
+		i++;
+	}
+	/*printf("rev a: %d | rev b: %d\n", best_comb[0], best_comb[1]);*/
+	execute_ops(a, b, best_comb[0], best_comb[1]);
+	push_b(a, b);
+}
+
+void turk_push_a(t_stack *a, t_stack *b)
+{
+	t_list *walk;
+	int i, j, sum;
+	int best_comb[2];
+
+	walk = b->head;
+	i = 0;
+	sum = a->size + b->size;
+	while (walk)
+	{
+		j = closest_bigger(a, get_value(walk));
+		if (j == -1)
+			j = smallest_index(a);
+		if (optimized_ops_nbr(a, b, j, i) < sum)
+		{
+			best_comb[0] = j;
+			best_comb[1] = i;
+			
+			// printf("{new best} a(i) = %d, b(i) = %d\n", j, i);
+			sum = optimized_ops_nbr(a, b, j, i);
+		}
+		if (sum == 0)
+			break;
+		walk = walk->next;
+		i++;
+	}
+	execute_ops(a, b, best_comb[0], best_comb[1]);
+	push_a(a, b);
+}
+
+void turk(t_stack *a, t_stack *b)
+{
+	int i;
+
 	push_b(a,  b);
 	push_b(a,  b);
-	if (get_value(b->head) < get_value(b->head->next))
-		swap_b(b);
-	best_fit(a,  b);
-	push_all_b_to_a(a, b);
+	i = a->size - 3;
+	while (i > 0)
+	{
+		cheapest_push(a, b);
+		i--;
+	}
+	sort3_a(a);
+	i = b->size;
+	while (i > 0)
+	{
+		turk_push_a(a, b);
+		i--;
+	}
+	/*cheapest_push(a, b);*/
+	/*print_ab(a, b);*/
+	/*cheapest_push(a, b);*/
+	/*print_ab(a, b);*/
+	int min_idx = smallest_index(a);
+	execute_ops(a, b, min_idx, 0);
+
 }
 
 int main(int ac, char **av)
 {		
 	t_stack stack_a, stack_b;
 	size_t size;
-	/*int bits_count;*/
 
 	size = ac - 1;
 	stack_a.head = arr_to_list(&av[1], size);;
@@ -639,7 +931,7 @@ int main(int ac, char **av)
 	stack_b.size = 0;
 
 	print_ab(&stack_a, &stack_b);
-	join(&stack_a, &stack_b);
+	turk(&stack_a, &stack_b);
 	print_ab(&stack_a, &stack_b);
 	ft_lstclear(&stack_a.head, free);
 }
