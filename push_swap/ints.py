09913b90ad1d0e20c51116835bec0d26d8a959e3
ila36IX <jbariali002@gmail.com>
2025-01-28 21:10:18 +0100

    Move turk source

--- push_swap/ints.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python3
-import sys, random
-
-if len(sys.argv) != 2:
-    exit(1)
-ints = random.sample(range(0, int(sys.argv[1])), int(sys.argv[1]))
-print(" ".join([str(i) for i in ints]))
-
